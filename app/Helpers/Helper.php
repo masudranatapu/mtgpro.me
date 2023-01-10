bb9e4e73ba1254aecd6f7549bd44136e3bd7124d
arifurrahmansw <arifurrahman91@live.com>
2023-01-10 12:09:04 +0600

    s

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -241,7 +241,7 @@ function checkPackage($id = null){
 
 
 function isFreePlan(){
-    $user = DB::table('users')->select('plans.is_free')->leftJoin('plans','users.plan_id','=','plans.id')->where('id',Auth::user()->id)->first();
+    $user = DB::table('users')->select('plans.is_free')->leftJoin('plans','plans.id','=','users.plan_id')->where('users.id',Auth::user()->id)->first();
     if($user->is_free==1){
         return true;
     }
