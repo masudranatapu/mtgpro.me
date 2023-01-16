41dbcbc765ab092616f37addcb36a972f25c653a
unknown <rabinmia7578@gmail.com>
2023-01-16 19:58:34 +0600

    update

--- resources/views/user/layouts/sidebar.blade.php
+++ resources/views/user/layouts/sidebar.blade.php
@@ -6,11 +6,11 @@ $settings = getSetting();
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('user.card') }}" class="brand-link">
         <span class="brand-text font-weight-light">
-        @if (!empty($settings->site_logo))
-        <img src="{{ asset($settings->site_logo) }}" width="150" alt="logo">
-        @else
-        <img src="{{ asset('assets/img/logo.png') }}" width="150" alt="logo">
-        @endif
+            @if (!empty($settings->site_logo))
+            <img src="{{ asset($settings->site_logo) }}" width="150" alt="logo">
+            @else
+            <img src="{{ asset('assets/img/logo.png') }}" width="150" alt="logo">
+            @endif
         </span>
     </a>
     <div class="sidebar">
@@ -35,7 +35,21 @@ $settings = getSetting();
                 <li class="nav-item">
                     <a href="{{ route('user.connections') }}" class="nav-link @yield('connections')">
                         <span class="icon">
-                            <svg width="16" height="16" fill="none" viewBox="0 0 16 15"><rect x="1.5" y="3.58136" width="13" height="10.0437" rx="2" stroke="#BDBDBD" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></rect><path fill-rule="evenodd" clip-rule="evenodd" d="M12.9511 1.72704H2.58203V1.375C2.58203 0.822715 3.02975 0.375 3.58203 0.375H11.9511C12.5034 0.375 12.9511 0.822715 12.9511 1.375V1.72704Z" fill="#BDBDBD"></path><path d="M8.00223 9.10522C8.77149 9.10522 9.39509 8.43071 9.39509 7.59866C9.39509 6.76661 8.77149 6.0921 8.00223 6.0921C7.23298 6.0921 6.60938 6.76661 6.60938 7.59866C6.60938 8.43071 7.23298 9.10522 8.00223 9.10522Z" stroke="#BDBDBD" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path><path d="M4.98438 13.6251C4.98438 12.1988 6.1425 11.1142 7.57111 11.1142H8.43335C9.86197 11.1142 11.0201 12.1988 11.0201 13.6251" stroke="#BDBDBD" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                            <svg width="16" height="16" fill="none" viewBox="0 0 16 15">
+                                <rect x="1.5" y="3.58136" width="13" height="10.0437" rx="2" stroke="#BDBDBD"
+                                    stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></rect>
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M12.9511 1.72704H2.58203V1.375C2.58203 0.822715 3.02975 0.375 3.58203 0.375H11.9511C12.5034 0.375 12.9511 0.822715 12.9511 1.375V1.72704Z"
+                                    fill="#BDBDBD"></path>
+                                <path
+                                    d="M8.00223 9.10522C8.77149 9.10522 9.39509 8.43071 9.39509 7.59866C9.39509 6.76661 8.77149 6.0921 8.00223 6.0921C7.23298 6.0921 6.60938 6.76661 6.60938 7.59866C6.60938 8.43071 7.23298 9.10522 8.00223 9.10522Z"
+                                    stroke="#BDBDBD" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round">
+                                </path>
+                                <path
+                                    d="M4.98438 13.6251C4.98438 12.1988 6.1425 11.1142 7.57111 11.1142H8.43335C9.86197 11.1142 11.0201 12.1988 11.0201 13.6251"
+                                    stroke="#BDBDBD" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round">
+                                </path>
+                            </svg>
                         </span>
                         {{ __('Connections') }}
                     </a>
@@ -58,9 +72,11 @@ $settings = getSetting();
                 </li>
                 @if (isAnnualPlan(Auth::user()->id))
                 <li class="nav-item">
-                    <a href="{{ route('user.free-marketing-material') }}" class="nav-link @yield('Free Marketing Material')">
+                    <a href="{{ route('user.free-marketing-material') }}"
+                        class="nav-link @yield('Free Marketing Material')">
                         <span class="icon">
-                            <img src="{{ asset('assets/img/icon/insights.svg') }}" alt="{{ __('Free Marketing Material') }}">
+                            <img src="{{ asset('assets/img/icon/insights.svg') }}"
+                                alt="{{ __('Free Marketing Material') }}">
                         </span>
                         {{ __('Free Marketing Material') }}
                     </a>
@@ -81,11 +97,13 @@ $settings = getSetting();
         <div class="user-panel align-items-center mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <a href="{{ route('user.setting') }}">
-                    <img src="{{ getAvatar(Auth::user()->profile_image) }}" class="img-circle elevation-2" alt="{{ Auth::user()->name }}">
+                    <img src="{{ getAvatar(Auth::user()->profile_image) }}" class="img-circle elevation-2"
+                        alt="{{ Auth::user()->name }}">
                 </a>
             </div>
             <div class="info">
-                <a href="{{ route('user.setting') }}" class="d-block" title="{{ Auth::user()->name }}">{{ Auth::user()->name }}</a>
+                <a href="{{ route('user.setting') }}" class="d-block" title="{{ Auth::user()->name }}">{{
+                    Auth::user()->name }}</a>
                 <span>{{ __('User') }}</span>
             </div>
         </div>
--- resources/views/user/plan/index.blade.php
+++ resources/views/user/plan/index.blade.php
@@ -8,21 +8,23 @@
 @section('meta_tag')@endsection
 @push('custom_css')
 <style>
-    .pricing-card{
+    .pricing-card {
         /* width: 319px; */
-    border: solid 1px #bdbdbd;
-    cursor: pointer;
-    height: 728px;
-    /* display: flex; */
-    padding: 24px;
-    margin-right: 15px;
-    border-radius: 20px;
-    background-color: #fff;
+        border: solid 1px #bdbdbd;
+        cursor: pointer;
+        height: 728px;
+        /* display: flex; */
+        padding: 24px;
+        margin-right: 15px;
+        border-radius: 20px;
+        background-color: #fff;
     }
-    .pricing-card:hover{
+
+    .pricing-card:hover {
         border: 1px solid #4B8CE2;
         filter: drop-shadow(0px 12px 24px rgba(0, 0, 0, 0.11));
     }
+
     .plan_type .form-check {
         position: relative;
         padding-left: 1.25rem;
@@ -31,7 +33,8 @@
         font-size: 14px;
         font-weight: 500;
     }
-    .current-plan-btn{
+
+    .current-plan-btn {
 
         color: rgba(0, 0, 0, 0.26);
         box-shadow: none;
@@ -68,14 +71,14 @@
                     </div>
                     <div class="plan_type switchBtn text-center mb-4 mt-3">
                         <div class="switch-wrapper">
-                                <input id="monthly" name="planType" value="annual" type="radio" name="switch" checked>
-                                <input id="yearly" name="planType" value="monthly" type="radio" name="switch">
-                                <label for="monthly">Annual</label>
-                                <label for="yearly">Monthly</label>
-                                <span class="highlighter"></span>
-                          </div>
+                            <input id="monthly" name="planType" value="annual" type="radio" name="switch" checked>
+                            <input id="yearly" name="planType" value="monthly" type="radio" name="switch">
+                            <label for="monthly">Annual</label>
+                            <label for="yearly">Monthly</label>
+                            <span class="highlighter"></span>
+                        </div>
 
-                       <!-- <div class="form-check form-switch d-inline">
+                        <!-- <div class="form-check form-switch d-inline">
                             <label class="form-check-label" for="CheckedAnnualy">
                                 <input class="form-check-input" name="planType" checked="" type="radio" value="annual" id="CheckedAnnualy">
                                     {{ __('Annual')}}
@@ -90,52 +93,73 @@
                     </div>
                 </div>
             </div>
-            <div class="row justify-content-md-center">
-                @if (!empty($plans) && count($plans) > 0)
-                    @foreach($plans as $plan)
-                @php
-                    $planfeatures = json_decode($plan->features);
-                @endphp
-                <div class="col-md-6 col-xl-3 @if($plan->plan_type == 1) solopreneur_and_individuals  @else team_accounts @endif">
-                    <div class="pricing-card card card-md">
-                        <div class="card-body text-center">
-                            <div class="text-capitalize text-dark font-weight-bold">  {{$plan->plan_name}}</div>
-                            <div class="text-center mt-4">
-                                @if ($user->plan_id==$plan->id && $plan->is_free==1)
-                                    {{-- <a href="javascript:void(0)" class="down-plan-model btn btn-danger w-100" title="{{ __('Active Plan')}}">{{ __('Active Plan')}}</a> --}}
-                                    <a href="javascript:void(0)" class="current-plan-btn btn-block btn-primary w-100" title="{{ __('Current Plan')}}">{{ __('Current Plan')}}</a>
-                                @elseif ($user->plan_id==$plan->id)
-                                    <a href="javascript:void(0)" class="current-plan-btn btn-block btn-primary w-100" data-href="{{ route('user.checkout') }}" data-id="{{ $plan->id }}" title="{{ __('Current plan')}}">{{ __('Choose plan')}}</a>
-                                @else
-                                    <a href="javascript:void(0)" class="choose-plan btn btn-primary btn-block btn-primary w-100" data-href="{{ route('user.checkout') }}" data-id="{{ $plan->id }}" title="{{ __('Choose plan')}}">{{ __('Choose plan')}}</a>
-                                @endif
-                            </div>
-                                <div class="my-3 pb-3">
-                                    <div class="price">
-                                        <h4 class="planpricemonthly">  {{ $currency->symbol }} {{$plan->plan_price_monthly}} <sub> / {{ __('Monthly')}} </sub></h4>
-                                        <h4 class="planpriceyearly">$ {{$plan->plan_price_yearly}} <sub> / {{ __('Yearly')}} </sub></h4>
+            <div class="row d-flex justify-content-md-center">
+                <div class="col-xl-11">
+                    <div class="row">
+                        @if (!empty($plans) && count($plans) > 0)
+                        @foreach($plans as $plan)
+                        @php
+                        $planfeatures = json_decode($plan->features);
+                        @endphp
+                        <div
+                            class="col-md-6 col-xl-4 @if($plan->plan_type == 1) solopreneur_and_individuals  @else team_accounts @endif">
+                            <div class="pricing-card card card-md">
+                                <div class="card-body text-center">
+                                    <div class="text-capitalize text-dark font-weight-bold"> {{$plan->plan_name}}</div>
+                                    <div class="text-center mt-4">
+                                        @if ($user->plan_id==$plan->id && $plan->is_free==1)
+                                        {{-- <a href="javascript:void(0)" class="down-plan-model btn btn-danger w-100"
+                                            title="{{ __('Active Plan')}}">{{ __('Active Plan')}}</a> --}}
+                                        <a href="javascript:void(0)"
+                                            class="current-plan-btn btn-block btn-primary w-100"
+                                            title="{{ __('Current Plan')}}">{{ __('Current Plan')}}</a>
+                                        @elseif ($user->plan_id==$plan->id)
+                                        <a href="javascript:void(0)"
+                                            class="current-plan-btn btn-block btn-primary w-100"
+                                            data-href="{{ route('user.checkout') }}" data-id="{{ $plan->id }}"
+                                            title="{{ __('Current plan')}}">{{ __('Choose plan')}}</a>
+                                        @else
+                                        <a href="javascript:void(0)"
+                                            class="choose-plan btn btn-primary btn-block btn-primary w-100"
+                                            data-href="{{ route('user.checkout') }}" data-id="{{ $plan->id }}"
+                                            title="{{ __('Choose plan')}}">{{ __('Choose plan')}}</a>
+                                        @endif
                                     </div>
+                                    <div class="my-3 pb-3">
+                                        <div class="price">
+                                            <h4 class="planpricemonthly"> {{ $currency->symbol }}
+                                                {{$plan->plan_price_monthly}}
+                                                <sub> / {{ __('Monthly')}} </sub>
+                                            </h4>
+                                            <h4 class="planpriceyearly">$ {{$plan->plan_price_yearly}} <sub> / {{
+                                                    __('Yearly')}}
+                                                </sub></h4>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <ul class="list-unstyled lh-lg ">
+                                        @foreach($planfeatures as $features)
+                                        <li class="py-2">
+                                            <span>{{$features}}</span>
+                                        </li>
+                                        @endforeach
+                                    </ul>
                                 </div>
-                                <hr>
-                                <ul class="list-unstyled lh-lg ">
-                            @foreach($planfeatures as $features)
-                                <li class="py-2">
-                                    <span>{{$features}}</span>
-                                </li>
-                                @endforeach
-                            </ul>
+                            </div>
                         </div>
+                        @endforeach
+                        @endif
                     </div>
                 </div>
-                @endforeach
-                @endif
-            </div>
+
             </div>
         </div>
     </div>
 </div>
-    <!-- Modal -->
-<div class="modal fade" id="planConfirmModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+</div>
+<!-- Modal -->
+<div class="modal fade" id="planConfirmModal" data-backdrop="static" data-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header border-0">
@@ -151,20 +175,23 @@
                     <label for="plan_id" class="sr-only">{{ __('Plan id')}}</label>
                     <input type="hidden" class="form-control" name="plan_id" value="" id="plan_id">
                     <div class="modal-title">{{ __('“We’re excited to have you!')}}</div>
-                    <div class="mb-2">{{ __('Proceed to checkout with this plan and you’ll be able to upgrade or downgrade at any time')}}</div>
+                    <div class="mb-2">{{ __('Proceed to checkout with this plan and you’ll be able to upgrade or
+                        downgrade at any time')}}</div>
                     <div class="text-danger">
-                            {{ __('For upgrading users, simply visit “Business Cards” to enable your cards after the upgrade')}}
+                        {{ __('For upgrading users, simply visit “Business Cards” to enable your cards after the
+                        upgrade')}}
                     </div>
                     <div class="modal-footer mt-4">
                         <button type="button" class="btn btn-danger" data-dismiss="modal">{{ __('Close')}}</button>
-                         <button type="submit" class="btn btn-primary">{{ __('Confirm')}}</button>
+                        <button type="submit" class="btn btn-primary">{{ __('Confirm')}}</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
 </div>
-<div class="modal animate__animated animate__fadeIn" id="downPlanModal" tabindex="-1" aria-labelledby="downPlanModalLabel" aria-hidden="true">
+<div class="modal animate__animated animate__fadeIn" id="downPlanModal" tabindex="-1"
+    aria-labelledby="downPlanModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-sm modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-body">
@@ -183,7 +210,7 @@
 @endsection
 @push('custom_js')
 <script>
-$(document).on('click', '.choose-plan', function () {
+    $(document).on('click', '.choose-plan', function () {
     var plan_id =  $(this).attr('data-id');
     $("#plan_id").val(plan_id);
     $('#planConfirmModal').modal('show');
