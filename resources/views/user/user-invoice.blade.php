c6cb3a17bf567bc965bd9d52806f328ea00a1c6c
Mridul <mridul>
2023-01-29 15:28:12 +0600

    cls

--- resources/views/user/user-invoice.blade.php
+++ resources/views/user/user-invoice.blade.php
@@ -60,62 +60,62 @@
         }
 
         /* .review_content p {
-                        margin: 0;
-                        font-size: 14px;
-                        text-align: justify;
-                        color: #5e5e5e;
-                        line-height: 26px;
-                        font-family: 'Poppins', sans-serif;
-                        font-weight: 400;
-                    }
-                    .edit_review {
-                        position: absolute;
-                        top: 8px;
-                        right: 13px;
-                        cursor: pointer;
-                    }
-                    .edit_review i {
-                        color: #BBB;
-                        font-size: 19px;
-                        transition: all 0.3s ease;
-                    }
+                            margin: 0;
+                            font-size: 14px;
+                            text-align: justify;
+                            color: #5e5e5e;
+                            line-height: 26px;
+                            font-family: 'Poppins', sans-serif;
+                            font-weight: 400;
+                        }
+                        .edit_review {
+                            position: absolute;
+                            top: 8px;
+                            right: 13px;
+                            cursor: pointer;
+                        }
+                        .edit_review i {
+                            color: #BBB;
+                            font-size: 19px;
+                            transition: all 0.3s ease;
+                        }
 
 
-                    .review_edit_form label {
-                        font-size: 14px;
-                        color: #3b3b3b;
-                        font-family: 'Poppins';
-                        font-weight: 400;
-                    }
+                        .review_edit_form label {
+                            font-size: 14px;
+                            color: #3b3b3b;
+                            font-family: 'Poppins';
+                            font-weight: 400;
+                        }
 
-                    .review_edit_form .form-control {
-                        height: 44px;
-                        border: 1px solid #DDD;
-                        outline: none;
-                        box-shadow: none;
-                        border-radius: 3px;
-                    }
-                    .review_edit_form .btn {
-                        padding: 6px 33px;
-                        border-radius: 2px;
-                        font-family: 'Poppins', sans-serif;
-                        font-weight: 400;
-                        font-size: 15px;
-                        border: none !important;
-                    }
-                    .status{
-                    margin: 20px;
-                    float: right;
-                    }
-                    .active{
-                        color: orange;
-                        font-weight: 600;
-                        font-family: 'Poppins', sans-serif;
-                    }
-                    .pending{
-                        font-weight: 600;
-                        font-family: 'Poppins', sans-serif;
-                    } */
+                        .review_edit_form .form-control {
+                            height: 44px;
+                            border: 1px solid #DDD;
+                            outline: none;
+                            box-shadow: none;
+                            border-radius: 3px;
+                        }
+                        .review_edit_form .btn {
+                            padding: 6px 33px;
+                            border-radius: 2px;
+                            font-family: 'Poppins', sans-serif;
+                            font-weight: 400;
+                            font-size: 15px;
+                            border: none !important;
+                        }
+                        .status{
+                        margin: 20px;
+                        float: right;
+                        }
+                        .active{
+                            color: orange;
+                            font-weight: 600;
+                            font-family: 'Poppins', sans-serif;
+                        }
+                        .pending{
+                            font-weight: 600;
+                            font-family: 'Poppins', sans-serif;
+                        } */
     </style>
 @endpush
 
