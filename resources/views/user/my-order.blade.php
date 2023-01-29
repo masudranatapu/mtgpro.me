749cdbe9eb9a9141ec2481b92ad9b5b87c851131
Mridul <mridul>
2023-01-29 16:00:37 +0600

    adas

--- resources/views/user/my-order.blade.php
+++ resources/views/user/my-order.blade.php
@@ -1,6 +1,6 @@
 @extends('user.layouts.app')
 
-@section('title') {{ __('My Order') }} @endsection
+@section('title') {{ __('My Order') }}  @endsection
 
 @push('custom_css')
     <style>
@@ -10,7 +10,6 @@
             border-radius: 1px;
             background: #fff !important;
         }
-
         .profile_info h3 {
             font-size: 17px;
             font-weight: 600;
@@ -18,20 +17,17 @@
             margin: 0;
             color: #070707;
         }
-
         .profile_info span {
             color: #888888;
             font-size: 14px;
             font-family: 'Inter';
             font-weight: 400;
         }
-
         .divider {
             border-bottom: 1px dashed #DDD;
             position: relative;
             margin: 23px 0px;
         }
-
         .divider i {
             position: absolute;
             top: -12px;
@@ -44,13 +40,11 @@
             width: 53px;
             color: orange;
         }
-
-        .custom_table {
+        .custom_table{
             background: rgb(255, 255, 255);
             padding: 30px;
             border-radius: 16px;
         }
-
         .custom_image {
             border: 1px solid #ddd;
             border-radius: 4px;
@@ -58,148 +52,148 @@
             width: 85px;
             height: 80px;
         }
-
         /* .review_content p {
-                                                                margin: 0;
-                                                                font-size: 14px;
-                                                                text-align: justify;
-                                                                color: #5e5e5e;
-                                                                line-height: 26px;
-                                                                font-family: 'Poppins', sans-serif;
-                                                                font-weight: 400;
-                                                            }
-                                                            .edit_review {
-                                                                position: absolute;
-                                                                top: 8px;
-                                                                right: 13px;
-                                                                cursor: pointer;
-                                                            }
-                                                            .edit_review i {
-                                                                color: #BBB;
-                                                                font-size: 19px;
-                                                                transition: all 0.3s ease;
-                                                            }
+            margin: 0;
+            font-size: 14px;
+            text-align: justify;
+            color: #5e5e5e;
+            line-height: 26px;
+            font-family: 'Poppins', sans-serif;
+            font-weight: 400;
+        }
+        .edit_review {
+            position: absolute;
+            top: 8px;
+            right: 13px;
+            cursor: pointer;
+        }
+        .edit_review i {
+            color: #BBB;
+            font-size: 19px;
+            transition: all 0.3s ease;
+        }
 
 
-                                                            .review_edit_form label {
-                                                                font-size: 14px;
-                                                                color: #3b3b3b;
-                                                                font-family: 'Poppins';
-                                                                font-weight: 400;
-                                                            }
+        .review_edit_form label {
+            font-size: 14px;
+            color: #3b3b3b;
+            font-family: 'Poppins';
+            font-weight: 400;
+        }
 
-                                                            .review_edit_form .form-control {
-                                                                height: 44px;
-                                                                border: 1px solid #DDD;
-                                                                outline: none;
-                                                                box-shadow: none;
-                                                                border-radius: 3px;
-                                                            }
-                                                            .review_edit_form .btn {
-                                                                padding: 6px 33px;
-                                                                border-radius: 2px;
-                                                                font-family: 'Poppins', sans-serif;
-                                                                font-weight: 400;
-                                                                font-size: 15px;
-                                                                border: none !important;
-                                                            }
-                                                            .status{
-                                                            margin: 20px;
-                                                            float: right;
-                                                            }
-                                                            .active{
-                                                                color: orange;
-                                                                font-weight: 600;
-                                                                font-family: 'Poppins', sans-serif;
-                                                            }
-                                                            .pending{
-                                                                font-weight: 600;
-                                                                font-family: 'Poppins', sans-serif;
-                                                            } */
+        .review_edit_form .form-control {
+            height: 44px;
+            border: 1px solid #DDD;
+            outline: none;
+            box-shadow: none;
+            border-radius: 3px;
+        }
+        .review_edit_form .btn {
+            padding: 6px 33px;
+            border-radius: 2px;
+            font-family: 'Poppins', sans-serif;
+            font-weight: 400;
+            font-size: 15px;
+            border: none !important;
+        }
+        .status{
+        margin: 20px;
+        float: right;
+        }
+        .active{
+            color: orange;
+            font-weight: 600;
+            font-family: 'Poppins', sans-serif;
+        }
+        .pending{
+            font-weight: 600;
+            font-family: 'Poppins', sans-serif;
+        } */
     </style>
 @endpush
 
-@section('my_order', 'active')
+@section('my_order','active')
 
 @section('content')
-    <div class="content-wrapper">
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">{{ __('My Order') }}</h1>
-                    </div>
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">{{ __('My Order') }}</h1>
                 </div>
             </div>
         </div>
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="card-body">
-                            <table class="table table-bordered">
-                                <thead class="text-center">
+    </div>
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card-body custom_table table-bordered table-responsive">
+                         <table class="table ">
+                            <thead>
+                                <tr>
+                                    <th scope="col">{{ __('Sl.No') }}</th>
+                                    <th scope="col">{{ __('Order Number') }}</th>
+                                    <th scope="col">{{ __('Quantity') }}</th>
+                                    <th scope="col">{{ __('Discount') }}</th>
+                                    <th scope="col">{{ __('Total Price') }}</th>
+                                    <th scope="col">{{ __('Payment Fee') }}</th>
+                                    <th scope="col">{{ __('Grand Total') }}</th>
+                                    <th scope="col">{{ __('Customer') }}</th>
+                                    <th scope="col">{{ __('Order Date') }}</th>
+                                    <th scope="col">{{ __('Payment Method') }}</th>
+                                    <th scope="col">{{ __('Payment Status') }}</th>
+                                    <th scope="col">{{ __('Status') }}</th>
+                                    <th scope="col">{{ __('Actions') }}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($productOrders as $key => $value)
                                     <tr>
-                                        <th>{{ __('Sl') }}</th>
-                                        <th>{{ __('Order Number') }}</th>
-                                        <th>{{ __('Quantity') }}</th>
-                                        <th>{{ __('Discount') }}</th>
-                                        <th>{{ __('Total Price') }}</th>
-                                        <th>{{ __('Payment Fee') }}</th>
-                                        <th>{{ __('Grand Total') }}</th>
-                                        <th>{{ __('Customer') }}</th>
-                                        <th>{{ __('Order Date') }}</th>
-                                        <th>{{ __('Payment Method') }}</th>
-                                        <th>{{ __('Payment Status') }}</th>
-                                        <th>{{ __('Status') }}</th>
-                                        <th>{{ __('Actions') }}</th>
+                                        <td>{{ $key + 1 }}</td>
+                                        <td>{{ $value->order_number }}</td>
+                                        <td>{{ $value->quantity }}</td>
+                                        <td>{{ $value->discount }}%</td>
+                                        <td>{{ $value->total_price }}</td>
+                                        <td>{{ $value->payment_fee }}</td>
+                                        <td>{{ $value->grand_total }}</td>
+                                        <td>{{ $value->user_name }}</td>
+                                        <td>{{ date('d M Y', strtotime($value->order_date)) }}</td>
+                                        <td>{{ $value->payment_method }}</td>
+                                        <td class="text-muted">
+                                            @if ($value->payment_status)
+                                                <span class="badge bg-orange">{{ __('Paid') }}</span>
+                                            @else
+                                                <span class="badge bg-red">{{ __('Pending') }}</span>
+                                            @endif
+                                        </td>
+                                        <td class="text-muted">
+                                            @if ($value->status == 1)
+                                                <span class="badge bg-red">{{ __('Prossing') }}</span>
+                                            @elseif ($value->status == 2)
+                                                <span class="badge bg-orange">{{ __('On The Way') }}</span>
+                                            @elseif ($value->status == 3)
+                                                <span class="badge bg-success">{{ __('Deliverd') }}</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <a href="{{ route('user.orders.invoice', $value->id) }}" class="btn btn-sm btn-success">Invoice<a>
+                                        </td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($productOrders as $key => $value)
-                                        <tr>
-                                            <td>{{ $key + 1 }}</td>
-                                            <td>{{ $value->order_number }}</td>
-                                            <td>{{ $value->quantity }}</td>
-                                            <td>{{ $value->discount }}%</td>
-                                            <td>{{ $value->total_price }}</td>
-                                            <td>{{ $value->payment_fee }}</td>
-                                            <td>{{ $value->grand_total }}</td>
-                                            <td>{{ $value->user_name }}</td>
-                                            <td>{{ date('d M Y', strtotime($value->order_date)) }}</td>
-                                            <td>{{ $value->payment_method }}</td>
-                                            <td class="text-muted">
-                                                @if ($value->payment_status)
-                                                    <span class="badge bg-orange">{{ __('Paid') }}</span>
-                                                @else
-                                                    <span class="badge bg-red">{{ __('Pending') }}</span>
-                                                @endif
-                                            </td>
-                                            <td class="text-muted">
-                                                @if ($value->status == 1)
-                                                    <span class="badge bg-red">{{ __('Prossing') }}</span>
-                                                @elseif ($value->status == 2)
-                                                    <span class="badge bg-orange">{{ __('On The Way') }}</span>
-                                                @elseif ($value->status == 3)
-                                                    <span class="badge bg-success">{{ __('Deliverd') }}</span>
-                                                @endif
-                                            </td>
-                                            <td>
-                                                <a href="{{ route('user.orders.invoice', $value->id) }}"
-                                                    class="btn btn-sm btn-success">Invoice<a>
-                                            </td>
-                                        </tr>
-                                        <!-- Modal -->
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                    <!-- Modal -->
+
+                                @endforeach
+                            </tbody>
+                         </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @push('custom_js')
+
 @endpush
