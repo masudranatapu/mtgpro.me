ead2eacee4dde0b8c9b9cb9d1c9b96db092beae1
Mridul <mridul>
2023-01-29 20:19:58 +0600

    push

--- resources/views/pages/shop_details.blade.php
+++ resources/views/pages/shop_details.blade.php
@@ -118,7 +118,7 @@ $rows = $data ?? [];
                             </form>
 
                             <div class="short_info mt-4">
-                                <p>{{ Str::limit($product->details, 220, '...') }}</p>
+                                <p>{!! Str::limit($product->details, 220, '...') !!}</p>
                             </div>
 
                         </div>
@@ -145,20 +145,12 @@ $rows = $data ?? [];
                         <div class="tab-pane fade show active" id="nav-home" role="tabpanel"
                             aria-labelledby="nav-home-tab">
                             <div class="single_pro_details">
-                                {{ $product->details }}
+                                {!! $product->details !!}
                             </div>
                         </div>
                         <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
                             <div class="single_pro_details">
-                                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a
-                                    piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard
-                                    McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of
-                                    the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through
-                                    the cites of the word in classical literature, discovered the undoubtable source. Lorem
-                                    Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
-                                    Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the
-                                    theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,
-                                    "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                                <p></p>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vitae veritatis
                                     dolorem, rem saepe quibusdam tempora, neque quo voluptatum officia omnis amet, vel
                                     quisquam excepturi. Quod, rem. Nam, repudiandae laudantium!</p>
