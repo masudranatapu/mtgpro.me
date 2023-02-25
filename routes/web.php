83c585d9b1bb416246f94369cae4922131f33405
Mridul <mridul>
2023-02-25 18:01:56 +0600

    push

--- routes/web.php
+++ routes/web.php
@@ -152,7 +152,7 @@ Route::group(['as' => 'user.', 'prefix' => 'user', 'namespace' => 'User', 'middl
     Route::post('/user-disclaimer-view', ['as' => 'disclaimer.update', 'uses' => 'UserController@userdisclaimerShow']);
     Route::post('/user-nmls-view', ['as' => 'nmls.update', 'uses' => 'UserController@userNmlsShow']);
     Route::post('/user-nmls-add', ['as' => 'nmls.add', 'uses' => 'UserController@userNmlsAdd']);
-    Route::post('/user-cradit-auth-view', ['as' => 'cradit_auth.update', 'uses' => 'UserController@userCraditAuthShow']);
+    Route::post('/user-credit-auth-view', ['as' => 'credit_auth.update', 'uses' => 'UserController@usercraditAuthShow']);
     Route::post('/user-quick-application-view', ['as' => 'quick.application.update', 'uses' => 'UserController@quickApplication']);
 });
 
