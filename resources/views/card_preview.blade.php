390e656e62703a7db9a3c573e6334016322ce812
Rony Mia <ronymia.tech@gmail.com>
2023-02-25 09:39:22 +0600

    ..

--- resources/views/card_preview.blade.php
+++ resources/views/card_preview.blade.php
@@ -411,13 +411,13 @@
 
                                 @if ($user->housing_logo_view == '1')
                                     <div class="col-4 col-md-3 mb-3">
-                                        <a target="_blank" href="" data-bs-toggle="modal"
+                                        <a target="_blank" href="https://nmlsconsumeraccess.org" data-bs-toggle="modal"
                                             data-bs-target="#equalHouseModal" aria-controls="false">
                                             <img style="border-radius: 15px; margin:0 auto; padding:10px; background:{{ $cardinfo->theme_color ?? '#EEAD12' }}"
                                                 class="img-fluid d-block mb-1"
                                                 src="{{ asset('assets/img/icon/house.svg') }}" alt=""
                                                 width="70" height="70">
-                                            <span>Equal Housing Logo</span>
+                                            <span>Equal Housing Opportunity</span>
                                         </a>
                                     </div>
                                 @endif
