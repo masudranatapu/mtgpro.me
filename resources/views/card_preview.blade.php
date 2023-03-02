d19c3dd18c64648eb8936e7c93d006bc7d90ab4d
Md Maidul Islam <maidul.tech@gmail.com>
2023-03-02 11:41:33 +0600

    ..

--- resources/views/card_preview.blade.php
+++ resources/views/card_preview.blade.php
@@ -261,6 +261,8 @@
     @if (checkPackageValidity($cardinfo->user_id) == false)
     @include('_plan_expired_error')
     @else
+
+    {{ $errors }}
     <div class="template">
         <div class="card_view_wrapper" style="background: #C6E4D2; min-height: 936px;">
             <div class="card_cover">
